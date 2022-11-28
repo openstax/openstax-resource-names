734c9ae75bd59ecbd4fecce8c0e10f3283904181
tom <tom.g.woodward@gmail.com>
2022-11-28 14:14:50 -0500

    restructure patterns

--- packages/orn-locator/src/index.ts
+++ packages/orn-locator/src/index.ts
@@ -1,18 +1,11 @@
-import memoize from 'lodash/fp/memoize';
-import * as pathToRegexp from 'path-to-regexp';
 import { patterns } from './ornPatterns';
 
-const compileMatcher = memoize((path: string) =>
-  pathToRegexp.match<any>(path, {decode: decodeURIComponent})
-);
-
 export const locate = async (orn: string) => {
-
-  for (const [path, resolver] of Object.entries(patterns)) {
-    const match = compileMatcher(path)(orn);
+  for (const pattern of Object.values(patterns)) {
+    const match = pattern.match(orn);
 
     if (match) {
-      return await resolver(match.params);
+      return await pattern.resolve(match.params);
     }
   }
 
--- packages/orn-locator/src/ornPatterns.ts
+++ packages/orn-locator/src/ornPatterns.ts
@@ -1,8 +1,31 @@
+import * as pathToRegexp from 'path-to-regexp';
+
+const makePattern = <P extends object, R>({pattern, resolve}: {pattern: string; resolve: (params: P) => R}) => ({
+  resolve,
+  format: pathToRegexp.compile<P>(pattern),
+  match: pathToRegexp.match<any>(pattern, {decode: decodeURIComponent}),
+});
 
 export const patterns = {
-  ['https\\://openstax.org/orn/library/:lang']: ({lang}: {lang: string}) => import('./resolvers/books').then(mod => mod.library(lang)),
-  ['https\\://openstax.org/orn/book/:id']: ({id}: {id: string}) => import('./resolvers/books').then(mod => mod.bookDetail(id)),
-  ['https\\://openstax.org/orn/book\\:subbook/:bookId\\::subbookId']: (params: any) => import('./resolvers/books').then(mod => mod.subbook(params)),
-  ['https\\://openstax.org/orn/book\\:page/:bookId\\::pageId']: (params: any) => import('./resolvers/books').then(mod => mod.page(params)),
-  ['https\\://openstax.org/orn/book\\:page\\:element/:bookId\\::pageId\\::elementId']: (params: any) => import('./resolvers/books').then(mod => mod.element(params)),
+  library: makePattern({
+    pattern: 'https\\://openstax.org/orn/library/:lang',
+    resolve: ({lang}: {lang: string}) => import('./resolvers/books').then(mod => mod.library(lang)),
+  }),
+  book: makePattern({
+    pattern: 'https\\://openstax.org/orn/book/:id',
+    resolve: ({id}: {id: string}) => import('./resolvers/books').then(mod => mod.bookDetail(id)),
+  }),
+  'book:subbook': makePattern({
+    pattern: 'https\\://openstax.org/orn/book\\:subbook/:bookId\\::subbookId',
+    resolve: (params: {bookId: string; subbookId: string}) => import('./resolvers/books').then(mod => mod.subbook(params)),
+  }),
+  'book:page': makePattern({
+    pattern: 'https\\://openstax.org/orn/book\\:page/:bookId\\::pageId',
+    resolve: (params: {bookId: string; pageId: string}) => import('./resolvers/books').then(mod => mod.page(params)),
+  }),
+  'book:page:element': makePattern({
+    pattern: 'https\\://openstax.org/orn/book\\:page\\:element/:bookId\\::pageId\\::elementId',
+    resolve: (params: {bookId: string; pageId: string; elementId: string}) => import('./resolvers/books').then(mod => mod.element(params)),
+  }),
 };
+
