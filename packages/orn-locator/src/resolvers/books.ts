23bbee03c76a8b1d398a901e5dfa54d9c3985573
tom <tom.g.woodward@gmail.com>
2023-03-24 14:07:26 -0400

    fix a type

--- packages/orn-locator/src/resolvers/books.ts
+++ packages/orn-locator/src/resolvers/books.ts
@@ -37,7 +37,7 @@ const libraries = [
   'pl',
 ];
 
-export type LibraryData = ReturnType<typeof library>;
+export type LibraryData = Awaited<ReturnType<typeof library>>;
 
 const libraryData = (language: string) => {
   return {
