c4bfd1a2d04491080d5bf6cd9bd08797ed39460d
tom <tom.g.woodward@gmail.com>
2024-11-15 19:25:27 -0500

    fix types

--- packages/orn-locator/src/resolvers/ancillaries.ts
+++ packages/orn-locator/src/resolvers/ancillaries.ts
@@ -18,13 +18,15 @@ export const search = async(query: string, _limit: number) => {
 
 function formatAncillaryData(data: any) {
   return {
-    id: data.id,
+    id: data.id as string,
     orn: `https://openstax.org/orn/ancillary/${data.id}`,
     type: 'ancillary' as const,
-    title: data.name,
-    description: data.description,
+    title: data.name as string,
+    description: data.description as string,
     ancillaryType: {
-      ...data.type,
+      name: data.type.name as string,
+      id: data.type.id as string,
+      timesamp: data.type.timestamp as string,
       ...('icon' in data.type ? {icon: {url: ancillariesHost + data.type.icon.url}}: {})
     },
     urls: {
