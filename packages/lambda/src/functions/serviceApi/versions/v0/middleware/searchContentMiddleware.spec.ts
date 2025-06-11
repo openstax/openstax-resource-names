ce7e9438f5498dc30d8ae02d4a75047bbfda51f1
tom <tom.g.woodward@gmail.com>
2025-06-11 14:32:25 -0400

    fix specs

--- packages/lambda/src/functions/serviceApi/versions/v0/middleware/searchContentMiddleware.spec.ts
+++ packages/lambda/src/functions/serviceApi/versions/v0/middleware/searchContentMiddleware.spec.ts
@@ -4,10 +4,10 @@ import { config, searchContentMiddleware } from './searchContentMiddleware';
 describe('searchMiddleware', () => {
   it('adds searchClient to middleware', () => {
     const searchClient = {};
-    const createSearchClient = jest.fn().mockReturnValue(searchClient);
+    const createSearchContentClient = jest.fn().mockReturnValue(searchClient);
     const request = {};
-    const middleware = searchContentMiddleware({createSearchClient} as unknown as AppServices)({request});
-    expect(createSearchClient).toHaveBeenCalledWith(config);
+    const middleware = searchContentMiddleware({createSearchContentClient} as unknown as AppServices)({request});
+    expect(createSearchContentClient).toHaveBeenCalledWith(config);
     expect(middleware.request).toEqual(request);
     expect(middleware.searchContentClient).toEqual(searchClient);
   });
